01f79f8d6b841e56c9f84e8f0fb2fe15eead56f5
shammils <skyler.hamilton@protonmail.com>
2021-08-18 19:49:44 -0700

    hajimeru

---
+++ yonde/index.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: '> '
+})
+
+rl.prompt()
+rl.on('line', handle)
+
+function handle(line) {
+  console.log(`you typed '${line}'`)
+}
