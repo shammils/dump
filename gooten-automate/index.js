e5963023ff32c43969ee7ec3d5264c366d0c9922
shammils <skyler.hamilton@protonmail.com>
2021-10-28 04:09:28 -0400

    removed unecessary code from code im not even going to use anymore. why am I commiting it

--- gooten-automate/index.js
+++ gooten-automate/index.js
@@ -129,14 +129,4 @@ const smallPadding = 500
   } finally {
     await driver.quit()
   }
-
-
-  driver.quit()
-  try {
-    await driver.get('http://www.google.com/ncr');
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-  } finally {
-    await driver.quit();
-  }
 })()
